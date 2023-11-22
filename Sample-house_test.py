9aebe417e070f1f4b54f69ad5df9cb7c035b4c66
Darragh Hoare <91789390+DarraghBonatti@users.noreply.github.com>
2023-11-22 16:02:57 +0000

    Add files via upload

---
+++ Sample-house_test.py
@@ -0,0 +1,15 @@
+import project.py
+
+murphy_household = new Household('Murphys House')
+
+living_room_sensor = new Sensor('Living Room', 'Radiator')
+murphy_household.add_room('Living Room', living_room_sensor)
+
+laura_bedroom_sensor = new Sensor('Living Room', 'Radiator')
+
+murphy_household.add_room('Lauras Bedroom', laura_bedroom_sensor)
+murphy_household.add_room('Kitchen', )
+
+
+
+# living_room_sensor.setTemp()
